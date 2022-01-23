f6a98236950b6902c4af662e0d8fe9e511cdfa24
JessChris <j.christodoulou@outlook.com>
2022-01-23 17:40:31 +0200

    Add project files.

---
+++ keyLoggercpp.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <windows.h>
+#include <winuser.h> //so I can use the GetAsyncKeyState function
+#include <fstream>
+using namespace std;
+
+//functions used:
+void start();
+void stealth();
+
+
+int main()
+{
+	cout << "hello";
+}
+
+
+void start()
+{
+	char characterInput;
+
+	for (;;) //an endless loop
+	{
+
+	}
+
+}
